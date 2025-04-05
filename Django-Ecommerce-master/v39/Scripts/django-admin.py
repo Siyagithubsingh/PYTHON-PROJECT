a00beb5f72f81ee2013b2f031cb28596d181813b
siya <ssiyasingh2002@gmail.com>
2025-04-05 23:22:55 +0530

    first commit

---
+++ Django-Ecommerce-master/v39/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\HP\Documents\ECOM\Django-Ecommerce-master\v39\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
